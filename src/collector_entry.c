897efc659dce89ce6d59e1bb88f5423ff392c541
flylin <linxiang1208@gmail.com>
2026-09-23 00:42:15 +0800

    fix: rename _process to ds_start (Mach-O adds _ prefix, matches group.html lookup)

--- src/collector_entry.c
+++ src/collector_entry.c
@@ -8,6 +8,6 @@ extern void ds_collect(void);
 
 /* Constructor - runs on dlopen by MachOPayloadBuilder */
 __attribute__((constructor))
-void _process(void) {
+void ds_start(void) {
     ds_collect();
 }
